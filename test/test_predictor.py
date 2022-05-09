a6501aabc0396af06a3e7c409f9381c1dfc04033
Ömer Erdinç Yağmurlu <omeryagmurlu@gmail.com>
2022-05-09 12:09:06 +0200

    added regression tests for tsfresh (in tandem with js, to make sure order is the same)

--- test/test_predictor.py
+++ test/test_predictor.py
@@ -1,8 +1,9 @@
 from io import StringIO
 import unittest
-import time
-from src.edgeml.predictor import Predictor, PredictorError
+import tsfresh
+from src.edgeml.predictor import Predictor
 from pandas.util.testing import assert_frame_equal
+from numpy.testing import assert_array_equal
 import pandas as pd
 
 class TestInterpolation(unittest.TestCase):
@@ -88,6 +89,66 @@ class TestInterpolation(unittest.TestCase):
 2022-05-08 21:06:51.265,5.0,11.0,42.0,0
 2022-05-08 21:06:52.265,5.0,12.0,43.0,0
 2022-05-08 21:06:53.265,5.0,12.0,44.0,0""")))
+
+class TestFeatureExtraction(unittest.TestCase):
+    def setUp(self):
+        _time = 1652044007265
+
+        _p = Predictor(
+            lambda input: 5,
+            ['ACC_RAW_z', 'ACC_RAW_y', 'ACC_RAW_x'],
+            5,
+            ['62536901f2e49867b6936d9b', '62536901f2e49867b6936d9c']
+        )
+
+        _z = [   5, None, None, None, None, None, None,   45, None,   49]
+        _y = [None, None,    9,   10,   11,   12, None,    1, None,   2]
+        _x = [   5,    6, None,   41,   42,   43,   44,   45,   46, None]
+
+        i = 0
+
+        while i < len(_x):
+            t = _time + i * 1000
+            _z[i] and _p.add_datapoint('ACC_RAW_z', float(_z[i]), t)
+            _y[i] and _p.add_datapoint('ACC_RAW_y', float(_y[i]), t)
+            _x[i] and _p.add_datapoint('ACC_RAW_x', float(_x[i]), t)
+            i = i+1
+
+        samples = Predictor._merge(_p.store)
+        self.interp = Predictor._interpolate(samples)
+
+    def test_tsfresh(self):
+        settings = tsfresh.feature_extraction.settings.MinimalFCParameters()
+        features = tsfresh.extract_features(
+            self.interp, column_id="id", default_fc_parameters=settings, n_jobs=0, disable_progressbar=True
+        )
+
+        # ordering
+        self.assertEqual(features.iloc[0].index.tolist(), [
+            'ACC_RAW_z__sum_values',
+            'ACC_RAW_z__median',
+            'ACC_RAW_z__mean',
+            'ACC_RAW_z__length',
+            'ACC_RAW_z__standard_deviation',
+            'ACC_RAW_z__variance',
+            'ACC_RAW_z__root_mean_square',
+            'ACC_RAW_z__maximum',
+            'ACC_RAW_z__absolute_maximum',
+            'ACC_RAW_z__minimum', 'ACC_RAW_y__sum_values', 'ACC_RAW_y__median', 'ACC_RAW_y__mean', 'ACC_RAW_y__length', 'ACC_RAW_y__standard_deviation', 'ACC_RAW_y__variance', 'ACC_RAW_y__root_mean_square', 'ACC_RAW_y__maximum', 'ACC_RAW_y__absolute_maximum', 'ACC_RAW_y__minimum', 'ACC_RAW_x__sum_values', 'ACC_RAW_x__median', 'ACC_RAW_x__mean', 'ACC_RAW_x__length', 'ACC_RAW_x__standard_deviation', 'ACC_RAW_x__variance', 'ACC_RAW_x__root_mean_square', 'ACC_RAW_x__maximum', 'ACC_RAW_x__absolute_maximum', 'ACC_RAW_x__minimum'])
+        # values
+        assert_array_equal(features.iloc[0].values.tolist(), [
+            296.0,
+            30.714285714285715,
+            29.6,
+            10.0,
+            14.89908913802643,
+            221.9828571428571,
+            33.13823859445244,
+            49.0,
+            49.0,
+            5.0, 71.0, 9.0, 7.1, 10.0, 3.916631205513228, 15.34, 8.108637370113428, 12.0, 12.0, 1.0, 341.5, 42.5, 34.15, 10.0, 15.614176251086706, 243.80250000000007, 37.55029959933742, 46.0, 46.0, 5.0]
+        ) 
+
 
 if __name__ == '__main__':
     unittest.main()
